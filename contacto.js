a02b0a46c8f5270ed6deec3b4426b5ef34eeb3be
acerrato92 <132726954+acerrato92@users.noreply.github.com>
2023-06-18 20:42:52 -0600

    se agrego contactos

---
+++ contacto.js
@@ -0,0 +1,10 @@
+const contactos = ["jonathan mendez","veronica cerrato","dilma cerrato","jesus mejivar","ina acosta","allison coronado","hector perez","isaac arteaga","brenda varela","jennifer ruiz"]
+
+const AgregarContactos =
+    contactos.push("maria cardenas");
+
+    const eliminarContactos =
+    contactos.splice(0,2);
+
+    const mostrarContactos =
+    console.log(contactos);
