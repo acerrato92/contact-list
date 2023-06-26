3fa276cd25d9d5ecab93dcbdcce0fbe915cecbb4
acerrato92 <132726954+acerrato92@users.noreply.github.com>
2023-06-26 00:51:03 -0600

    actualizo contactos

--- contacto.js
+++ contacto.js
@@ -1,10 +1,46 @@
-const contactos = ["jonathan mendez","veronica cerrato","dilma cerrato","jesus mejivar","ina acosta","allison coronado","hector perez","isaac arteaga","brenda varela","jennifer ruiz"]
+const contactos = [
+    {
+        id:2664714,
+        nombre: "jonathan enrrique",
+        apellidos: "mendez",
+        telefono: +50659645216,
+        ubicacion: "costarica",
+        ciudad: "alajuela",
+        direccion: "calle central frente a optica jimenez",
 
-const AgregarContactos =
-    contactos.push("maria cardenas");
+    },
+    {
+        id:544212,
+        nombre: "jesus alberto",
+        apellidos: "cerrato menjivar",
+        telefono: +50497927756,
+        ubicacion: "honduras",
+        ciudad: "la lima",
+        direccion: "calle principal tercer pasaje",
 
-    const eliminarContactos =
-    contactos.splice(0,2);
+    },
+    {
+        id:4536231,
+        nombre: "dilma veronica",
+        apellidos: "arteaga",
+        telefono: +50465489816,
+        ubicacion: "honduras",
+        ciudad: "san pedro sula",
+        direccion: "calle 6 bloque 4",
 
-    const mostrarContactos =
-    console.log(contactos);
+    },
+    {
+        id:35425,
+        nombre: "jorge enrrique",
+        apellidos: "varela galindo",
+        telefono: +50459645416,
+        ubicacion: "honduras",
+        ciudad: "chamelecon",
+        direccion: "entrada principal 3 calle 5 pasaje",
+
+    },
+    ];
+  
+    for (let i = 0; i < contactos.length; i++) {
+        console.log(contactos[2])
+    }
